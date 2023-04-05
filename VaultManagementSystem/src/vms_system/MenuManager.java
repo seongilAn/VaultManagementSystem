b3f2fa5c7ce0a2ae5dca4c44c9de3940526068ea
seongilAn <2022011799@gnu.ac.kr>
2023-04-05 17:55:59 +0900

    change name to MenuManager, make menu printing code

---
+++ VaultManagementSystem/src/vms_system/MenuManager.java
@@ -0,0 +1,11 @@
+package vms_system;
+
+public class MenuManager {
+	public static void main(String[] args) {
+		System.out.println("1. Add items");
+		System.out.println("2. Delete items");
+		System.out.println("3. Edit items");
+		System.out.println("4. View items");
+		System.out.println("5. Exit");
+	}
+}
--- VaultManagementSystem/src/vms_system/VaultManagementSystem.java
+++
@@ -1,7 +0,0 @@
-package vms_system;
-
-public class VaultManagementSystem {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
-}
